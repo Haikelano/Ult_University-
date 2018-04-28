20725f58093353cb15da731c8dc505bc297d939d
Haikelano <haykel.maaoui@gmail.com>
2018-04-29 00:39:27 +0200

    coriger ereur de la page student

--- UltUniversity/Models/Student.cs
+++ UltUniversity/Models/Student.cs
@@ -16,7 +16,7 @@ namespace UltUniversity.Models
         public string LastName { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        [Column("FirstName")]
+        
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
